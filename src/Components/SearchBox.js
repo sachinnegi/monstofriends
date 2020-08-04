f7c39b087bccdbd87003b3cfdb2d19b030394624
sachin negi <negisachin688@gmail.com>
2020-08-04 22:44:03 +0530

    latest changes

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
-        placeholder='search robots'
+        placeholder='search mosters'
         onChange={searchChange}
       />
     </div>
