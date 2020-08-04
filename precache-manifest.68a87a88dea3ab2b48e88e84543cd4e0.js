ca0b9b8841b2511c51f51e51bd3f8d1e75f7a2c9
sachin negi <negisachin688@gmail.com>
2020-08-04 22:44:48 +0530

    Updates

---
+++ precache-manifest.68a87a88dea3ab2b48e88e84543cd4e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b47a4658d835a12d0626b8b727238db",
+    "url": "/monstofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/monstofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d7a540608632d7e1716",
+    "url": "/monstofriends/static/css/main.294fc434.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/monstofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7a540608632d7e1716",
+    "url": "/monstofriends/static/js/main.5f29c239.chunk.js"
+  },
+  {
+    "revision": "ce954d5b621075384303",
+    "url": "/monstofriends/static/js/runtime-main.ca987d4b.js"
+  }
+]);
